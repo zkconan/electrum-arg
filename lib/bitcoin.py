f5baf31b78ffd1d778b60b2c6152261081e3ec72
protonn <support@argentum.io>
2016-10-28 23:09:42 -0500

    Fix for private key import

--- lib/bitcoin.py
+++ lib/bitcoin.py
@@ -310,12 +310,12 @@ def PrivKeyToSecret(privkey):
     return privkey[9:9+32]
 
 
-def SecretToASecret(secret, compressed=False, addrtype=23):
+def SecretToASecret(secret, compressed=False, addrtype=0):
     vchIn = chr((addrtype+128)&255) + secret
     if compressed: vchIn += '\01'
     return EncodeBase58Check(vchIn)
 
-def ASecretToSecret(key, addrtype=23):
+def ASecretToSecret(key, addrtype=0):
     vch = DecodeBase58Check(key)
     if vch and vch[0] == chr((addrtype+128)&255):
         return vch[1:]
