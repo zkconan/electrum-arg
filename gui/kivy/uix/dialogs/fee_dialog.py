0e954a5033dd908c0399d1f1cf6d67043989441b
protonn <support@argentum.io>
2017-04-22 16:14:05 -0500

    set dynamic fee false [kivy]

--- gui/kivy/uix/dialogs/fee_dialog.py
+++ gui/kivy/uix/dialogs/fee_dialog.py
@@ -61,7 +61,7 @@ class FeeDialog(Factory.Popup):
         self.fee_step = self.config.max_fee_rate() / 10
         self.fee_rate = self.config.fee_per_kb()
         self.callback = callback
-        self.dynfees = self.config.get('dynamic_fees', True)
+        self.dynfees = self.config.get('dynamic_fees', False)
         self.ids.dynfees.active = self.dynfees
         self.update_slider()
         self.update_text()
